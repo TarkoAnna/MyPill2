7826e6739f8085e49979b6a71ca6d41d0bc6202a
p5ych0p41h <a.s.ulmasov@yandex.ru>
2022-05-30 23:41:35 +0300

    editing gradle and calendar fragment

--- app/src/main/java/com/diplom/mypill2/ui/calendar/CalendarFragment.java
+++ app/src/main/java/com/diplom/mypill2/ui/calendar/CalendarFragment.java
@@ -1,41 +1,67 @@
 package com.diplom.mypill2.ui.calendar;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.diplom.mypill2.R;
 import com.diplom.mypill2.databinding.FragmentCalendarBinding;
-import com.diplom.mypill2.databinding.FragmentCalendarBinding;
+
+import java.util.Calendar;
 
 public class CalendarFragment extends Fragment {
 
     private @NonNull FragmentCalendarBinding binding;
+    private TextView textViewFirstDate;
+    private TextView textViewSecondDate;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        CalendarViewModel slideshowViewModel = new ViewModelProvider(this).get(CalendarViewModel.class);
         binding = FragmentCalendarBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        CalendarView calendarView = view.findViewById(R.id.calendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        textViewFirstDate = view.findViewById(R.id.text_view_first_date);
+        textViewSecondDate = view.findViewById(R.id.text_view_second_date);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        textViewFirstDate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                int mYear = year;
-                int mMonth = month;
-                int mDay = dayOfMonth;
-                String selectedDate = new StringBuilder().append(mMonth + 1)
-                        .append("-").append(mDay).append("-").append(mYear)
-                        .append(" ").toString();
-                Toast.makeText(getActivity().getBaseContext(), selectedDate, Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int day) {
+                            month += 1;
+                            String date = day + "/" + month + "/" + year;
+                            textViewFirstDate.setText(date);
+                        }
+                    }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+        textViewSecondDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int day) {
+                                month += 1;
+                                String date = day + "/" + month + "/" + year;
+                                textViewSecondDate.setText(date);
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
             }
         });
 
